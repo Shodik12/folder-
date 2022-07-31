fafe4143cc4c62578b28f0169cf72829defce8dd
Shodik12 <67968446+Shodik12@users.noreply.github.com>
2022-07-31 10:42:41 +0700

    Create file1.php

---
+++ file1.php
@@ -0,0 +1,163 @@
+<p><b>VI. PENILAIAN SENDIRI</b><div><b><br></b><div>
+
+</div><div>1. BPR wajib melakukan penilaian sendiri (self assessment) atas 
+
+</div><div>penerapan Tata Kelola sebagaimana dimaksud dalam Pasal 77 POJK 
+
+</div><div>Tata Kelola BPR paling sedikit 1 (satu) kali dalam setahun. Hasil 
+
+</div><div>penilaian sendiri (self assessment) atas penerapan Tata Kelola 
+
+</div><div>merupakan bagian tidak terpisahkan dari Laporan Penerapan Tata 
+
+</div><div>Kelola.
+
+</div><div>2. Penilaian terhadap pelaksanaan prinsip-prinsip Tata Kelola, paling 
+
+</div><div>sedikit diwujudkan dan difokuskan dalam 11 (sebelas) Faktor 
+
+</div><div>Penilaian Penerapan Tata Kelola yang terdiri atas:</div><div><br></div><div>a. Pelaksanaan tugas dan tanggung jawab Direksi;
+
+</div><div>b. Pelaksanaan tugas dan tanggung jawab Dewan Komisaris;
+
+</div><div>c. Kelengkapan dan pelaksanaan tugas atau fungsi Komite;
+
+</div><div>d. Penanganan benturan kepentingan;
+
+</div><div>e. Penerapan fungsi kepatuhan;
+
+</div><div>f. Penerapan fungsi audit intern;
+
+</div><div>g. Penerapan fungsi audit ekstern;
+
+</div><div>h. Penerapan manajemen risiko, termasuk sistem pengendalian 
+
+</div><div>intern;
+
+</div><div>i. Batas maksimum pemberian kredit;
+
+</div><div>j. Rencana bisnis BPR; dan
+
+</div><div>k. Transparansi kondisi keuangan dan non keuangan.
+
+</div><div>3. Dalam melaksanakan penilaian sendiri (self assessment), BPR 
+
+</div><div>menggunakan Pedoman Penilaian Penerapan Tata Kelola yang terdiri 
+
+</div><div>atas: 
+
+</div><div>a. Penjelasan Umum Pedoman Penilaian Penerapan Tata Kelola;
+
+</div><div>b. Tata Cara Pengisian Kertas Kerja Penilaian Penerapan Tata 
+
+</div><div>Kelola;
+
+</div><div>c. Kertas Kerja Penilaian Penerapan Tata Kelola; dan
+
+</div><div>d. Kesimpulan,
+
+</div><div>sebagaimana pada Lampiran II, yang merupakan bagian tidak 
+
+</div><div>terpisahkan dari Surat Edaran Otoritas Jasa Keuangan ini.
+
+</div><div>4. Pengisian Kertas Kerja Penilaian Penerapan Tata Kelola dilakukan 
+
+</div><div>dengan metode kualitatif berdasarkan data dan informasi yang sesuai 
+
+</div><div>keadaan yang sebenarnya.
+
+</div><div>5. Data dan informasi sebagaimana dimaksud pada angka 4 antara lain 
+
+</div><div>mencakup data kepengurusan, data kepemilikan, laporan tahunan
+
+</div><div>BPR, laporan pokok-pokok pelaksanaan tugas anggota Direksi yang 
+
+</div><div>membawahkan fungsi kepatuhan, laporan yang berkaitan dengan 
+
+</div><div>tugas fungsi Audit Intern pada BPR, laporan profil risiko, dokumen 
+
+</div><div>rencana bisnis dan realisasi rencana bisnis, laporan Dewan 
+
+</div><div>Komisaris dan laporan lain yang terkait Faktor Penilaian Penerapan 
+
+</div><div>Tata Kelola lainnya.
+
+</div><div>6. Hasil akhir penilaian sendiri (self assessment) penerapan Tata Kelola</div><div>bagi BPR berupa Peringkat Komposit dengan rentang penilaian 
+
+</div><div>sebagai berikut.</div><div><br></div><div><img src=" https://i.ibb.co/kx8WB19/27.jpg" style="width: 298px;"><br></div><div><br></div><div>7. Apabila terdapat salah satu faktor sebagaimana dimaksud pada
+
+</div><div>angka 2 yang seluruh kriteria atau indikatornya mendapatkan nilai 
+
+</div><div>Tidak Baik (5), Peringkat Komposit tertinggi yang dapat dicapai BPR 
+
+</div><div>adalah Cukup Baik.
+
+</div><div>8. Kertas Kerja Penilaian Penerapan Tata Kelola dan dokumen 
+
+</div><div>pendukung Kertas Kerja Penilaian Penerapan Tata Kelola, harus 
+
+</div><div>didokumentasikan dengan baik sehingga memudahkan penelusuran 
+
+</div><div>oleh pihak-pihak yang berkepentingan.
+
+</div><div>9. Berdasarkan Kertas Kerja Penilaian Penerapan Tata Kelola, BPR 
+
+</div><div>membuat Kesimpulan Umum Hasil Penilaian Penerapan Tata Kelola 
+
+</div><div>pada lembar tersendiri sebagaimana pada Lampiran III yang 
+
+</div><div>merupakan bagian tidak terpisahkan dari Surat Edaran Otoritas Jasa 
+
+</div><div>Keuangan ini, yang menggambarkan pemenuhan kecukupan seluruh 
+
+</div><div>Faktor Penilaian Penerapan Tata Kelola, paling sedikit meliputi:
+
+</div><div>a. Nilai Komposit dan Peringkat Komposit;
+
+</div><div>b. kelemahan dan penyebabnya, rencana tindak (action plan) yang
+
+</div><div>merupakan tindakan korektif (corrective action) serta target
+
+</div><div>waktu pelaksanaannya; dan
+
+</div><div>c. kekuatan penerapan Tata Kelola.
+
+</div><div>10. Kesimpulan Umum Hasil Penilaian Penerapan Tata Kelola dimaksud
+
+</div><div>harus ditandatangani oleh Direktur Utama dan Komisaris Utama 
+
+</div><div>BPR.
+
+</div><div>11. Laporan hasil penilaian sendiri (self assessment) Penerapan Tata 
+
+</div><div>Kelola untuk pertama kali yaitu posisi laporan akhir bulan Desember 
+
+</div><div>tahun 2016 disampaikan kepada Otoritas Jasa Keuangan, meliputi: 
+
+</div><div>Kertas Kerja Penilaian Penerapan Tata Kelola sebagaimana dimaksud 
+
+</div><div>dalam Lampiran II dan Kesimpulan Umum Hasil Penilaian Penerapan 
+
+</div><div>Tata Kelola sebagaimana dimaksud dalam Lampiran III yang</div><div>merupakan bagian tidak terpisahkan dari Surat Edaran Otoritas Jasa 
+
+</div><div>Keuangan ini .
+
+</div><div>12. Laporan hasil penilaian sendiri (self assessment) Penerapan Tata 
+
+</div><div>Kelola untuk tahun berikutnya meliputi laporan sebagaimana 
+
+</div><div>dimaksud pada angka 11 dan apabila berdasarkan hasil penilaian 
+
+</div><div>atau evaluasi oleh Otoritas Jasa Keuangan terdapat permintaan dari 
+
+</div><div>Otoritas Jasa Keuangan untuk melaksanakan rencana tindak, dalam 
+
+</div><div>Kesimpulan Umum Hasil Penilaian Penerapan Tata Kelola ditambah 
+
+</div><div>dengan informasi mengenai realisasi pencapaian pelaksanaan 
+
+</div><div>rencana tindak serta waktu penyelesaian dan kendala 
+
+</div><div>penyelesaiannya.
+
+</div><div><br></div></div></p>
